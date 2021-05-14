75c7cc843676441bdaf0bbd860ef46edb9a0cbbb
mzwandile <stuurmanmzwandile@gmail.com>
2021-05-14 15:42:12 +0200

    third commit

--- navigation.js
+++ navigation.js
@@ -1,2 +1,9 @@
-document.write('
- ');
+function classToggle() {
+  const navs = document.querySelectorAll(".Nav-bar");
+
+  navs.forEach((nav) => nav.classList.toggle("Navbar__ToggleShow"));
+}
+
+document
+  .querySelector(".Navbar__Link-toggle")
+  .addEventListener("click", classToggle);
